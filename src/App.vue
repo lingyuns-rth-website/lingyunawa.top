fb2c614c5b21286399adcf22f4f98452d22356e9
JingHai-Lingyun <Jim.Lin.4549@outlook.com>
2024-11-15 01:35:19 +0800

    fix: background url mismatch

--- src/App.vue
+++ src/App.vue
@@ -28,7 +28,7 @@
         width: 100vw;
         height: 100vh;
         background-image: url("https://lingyunawa.top/images/background.webp");
-        background-image: url("/images/background.webp");
+        // background-image: url("/images/background.webp");
         background-repeat: no-repeat;
         background-size: cover;
         z-index: -1;
